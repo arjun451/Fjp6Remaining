78ca7457322222a75371cc32d2ee7cec1b39615e
arjun451 <aksingh25071998@gmail.com>
2022-09-04 11:35:59 +0530

    ticket added using function

--- 3.Module/JIRa/script.js
+++ 3.Module/JIRa/script.js
@@ -1,5 +1,7 @@
 let addTag = document.querySelector(".add");
 let module= document.querySelector(".module-cont");
+let textArea = document.querySelector("#Task");
+let mainTag = document.querySelector(".main-cont");
 let flage = true;
 
 addTag.addEventListener('click',function(){
@@ -13,3 +15,29 @@ addTag.addEventListener('click',function(){
     }
     flage = !flage;
 })
+
+module.addEventListener("keydown",function(e){
+    let keyValue = e.key;
+    if(keyValue=="Enter")
+    {
+        creatTicket(textArea.value);
+        
+        textArea.value = "";
+        module.style.display = "none";
+
+       
+
+    }
+    flage = !flage;
+})
+
+function creatTicket(value)
+{
+     
+    let divTag = document.createElement("div");
+    divTag.setAttribute("class","ticket-cont");
+    divTag.innerHTML = `<div class="ticket-color "></div>
+    <div class="ticket-id">#12354DF</div>
+    <div class="ticket-area">${value}</div>`;
+   mainTag.appendChild(divTag);
+}
