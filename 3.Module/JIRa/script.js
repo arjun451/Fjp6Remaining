bdf0e45eb29e213cc0a5ae36e253b69ab4e669c5
arjun451 <aksingh25071998@gmail.com>
2022-09-04 12:51:03 +0530

    ticket added with color priority

--- 3.Module/JIRa/script.js
+++ 3.Module/JIRa/script.js
@@ -2,8 +2,11 @@ let addTag = document.querySelector(".add");
 let module= document.querySelector(".module-cont");
 let textArea = document.querySelector("#Task");
 let mainTag = document.querySelector(".main-cont");
+let priorityTag = document.querySelectorAll(".color");
 let flage = true;
+let color = "orange";
 
+//-----------------------
 addTag.addEventListener('click',function(){
     if(flage)
     {
@@ -15,12 +18,25 @@ addTag.addEventListener('click',function(){
     }
     flage = !flage;
 })
+//------------------------------------
+for(let i=0;i<priorityTag.length;i++)
+{
+    priorityTag[i].addEventListener('click',function(){
+        for(let j=0;j<priorityTag.length;j++)
+        {
+            priorityTag[j].classList.remove("active");
+        }
+        priorityTag[i].classList.add("active");
+        color = priorityTag[i].classList[0];
+    })
+}
 
+//----------------------
 module.addEventListener("keydown",function(e){
     let keyValue = e.key;
     if(keyValue=="Enter")
     {
-        creatTicket(textArea.value);
+        creatTicket(textArea.value,color);
         
         textArea.value = "";
         module.style.display = "none";
@@ -30,14 +46,14 @@ module.addEventListener("keydown",function(e){
     }
     flage = !flage;
 })
-
-function creatTicket(value)
+//----------------------------------------
+function creatTicket(value,color)
 {
      
     let divTag = document.createElement("div");
     divTag.setAttribute("class","ticket-cont");
-    divTag.innerHTML = `<div class="ticket-color "></div>
-    <div class="ticket-id">#12354DF</div>
+    divTag.innerHTML = `<div class="ticket-color ${color}"></div>
+    <div class="ticket-id ">#12354DF</div>
     <div class="ticket-area">${value}</div>`;
    mainTag.appendChild(divTag);
 }
