5fceceec8b4ed61083166b38a6675927309f8359
arjun451 <aksingh25071998@gmail.com>
2022-09-22 21:41:21 +0530

    StairPathVariableJump with memozation

--- DP/Lect002/StairPathVariableJump.java
+++ DP/Lect002/StairPathVariableJump.java
@@ -16,11 +16,26 @@ class StairPathVariableJump{
         return res;
     }
     //stairPathVariable with memozation
+    public static int StairPathVariableJumWithMemo(int n,int[] memo)
+    {
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+        int res=0;
+        for(int i=1;i<=n;i++)
+        {
+         res+=(memo[n-i]!=0)?memo[i]:StairPathVariableJumWithMemo(n-i,memo);
+        }
+        memo[n]=res;
+        return res;
+    }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         scn.close();
         System.out.println(StairPathVariable(n));
+        System.out.println(StairPathVariableJumWithMemo(n,new int[n+1]));
 
     }
 }
