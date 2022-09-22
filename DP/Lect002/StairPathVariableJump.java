1755036ecfe0d707b6c2da8cd3e76d990248a7bb
arjun451 <aksingh25071998@gmail.com>
2022-09-22 23:37:41 +0530

    Update logic

--- DP/Lect002/StairPathVariableJump.java
+++ DP/Lect002/StairPathVariableJump.java
@@ -1,41 +1,65 @@
 import java.util.Scanner;
-
 class StairPathVariableJump{
     //stairPathVariable jump recurision
-    public static int StairPathVariable(int n) {
+    public static int StairPathVariable(int src,int[] ar) {
         
-        if(n==0||n==1)
+        if(src==ar.length)
         {
             return 1;
         }
-        int res=0;
-        for(int i=1;i<=n;i++)
+        if(src>ar.length||ar[src]==0)
         {
-            res+=StairPathVariable(n-i);
+            return 0;
+        }
+        int res=0;
+        for(int i=1;i<=ar[src];i++)
+        {
+            res+=StairPathVariable(src+i,ar);
         }
         return res;
     }
     //stairPathVariable with memozation
-    public static int StairPathVariableJumWithMemo(int n,int[] memo)
+    public static int StairPathVariableJumWithMemo(int src,int dest,int[] ar,int[] memo )
     {
-        if(n==0||n==1)
+        if(src==dest)
         {
             return 1;
         }
-        int res=0;
-        for(int i=1;i<=n;i++)
+        if(memo[src]!=-1)
         {
-         res+=(memo[n-i]!=0)?memo[i]:StairPathVariableJumWithMemo(n-i,memo);
+            return memo[src];
         }
-        memo[n]=res;
-        return res;
+         
+        int res=0;
+        for(int i=1;i<=ar[src];i++)
+        {
+            if(i+src<=dest)
+            {
+            res+=StairPathVariableJumWithMemo(src+i,dest,ar,memo);
+            }
+        }
+         
+        return memo[src]=res;
     }
+
+     
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        int[] ar = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            ar[i] = scn.nextInt();
+        }
         scn.close();
-        System.out.println(StairPathVariable(n));
-        System.out.println(StairPathVariableJumWithMemo(n,new int[n+1]));
-
+        int[] memo = new int[n+1];
+         for(int i=0;i<=n;i++)
+         {
+            memo[i]=-1;
+         }
+         
+        System.out.println(StairPathVariable(0,ar));
+        System.out.println(StairPathVariableJumWithMemo(0,n,ar, memo));
+        
     }
 }
