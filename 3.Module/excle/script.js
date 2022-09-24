7e92664a02a3101de033c4ed1d27ce067462fbb1
arjun451 <aksingh25071998@gmail.com>
2022-09-25 02:41:22 +0530

    scrolls are fixed

--- 3.Module/excle/script.js
+++ 3.Module/excle/script.js
@@ -33,3 +33,17 @@ function initCell()
 }
 //call function
 initCell();
+
+let topRow = document.querySelector(".top-row");
+let LC = document.querySelector(".left-column");
+let TLC = document.querySelector(".top-left-cell");
+
+cellDiv.addEventListener("scroll",function(e){
+    
+  let scrollTop = e.target.scrollTop;
+  let scrollLet = e.target.scrollLeft;
+   topRow.style.top = scrollTop+"px";
+  LC.style.left = scrollLet+"px";
+  TLC.style.top = scrollTop+"px";
+  TLC.style.left = scrollLet+"px";
+});
